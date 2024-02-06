ceccb00f4fa6322a1c1f48e25d53cd6474801c41
victortwin <lifterr@mail.ru>
2024-02-06 19:05:31 +0400

    add tabBar controller and two navigation colntrollers

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Victor Tormyshev on 06.02.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,19 +1,16 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
-//  Created by Victor Tormyshev on 05.02.2024.
+//  Created by Victor Tormyshev on 06.02.2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
     }
-
-
 }
-
