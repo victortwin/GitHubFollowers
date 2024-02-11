97e6faa4f4191af3c432af4f3c0dae1490376acf
victortwin <lifterr@mail.ru>
2024-02-11 15:00:35 +0400

    Modal & passing data

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,28 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Victor Tormyshev on 11.02.2024.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username!)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+ 
+
+}
