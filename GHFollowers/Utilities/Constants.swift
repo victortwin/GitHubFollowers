2b812b53966624d80368afcf5ff40a3bb3070f9c
victortwin <lifterr@mail.ru>
2024-02-11 18:56:44 +0400

    Added child VC - USerInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Victor Tormyshev on 11.02.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
