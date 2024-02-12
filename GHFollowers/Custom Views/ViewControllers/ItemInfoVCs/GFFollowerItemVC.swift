b17bbcd3eba59a0576aaf529e682d2e4d00f4b61
victortwin <lifterr@mail.ru>
2024-02-12 20:53:21 +0400

    Implement UserInfo screen

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Victor Tormyshev on 12.02.2024.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
